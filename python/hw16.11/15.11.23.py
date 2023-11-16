21716b991f72a94f5f1b604d0a8cf291f67045a8
Aikhan2512 <150243233+Aikhan2512@users.noreply.github.com>
2023-11-16 14:52:42 +0600

    Create 15.11.23.py

---
+++ python/hw16.11/15.11.23.py
@@ -0,0 +1,65 @@
+# person = {
+#     'ferst_name':'Aikhan',
+#     'last_name':'oralbek',
+#     'age':16,
+#     'country':'Kazakstan',
+#     'color':'yellou',
+#     'is_marred':True,
+#     'skills':['javaScript','pyton','html','Ract','node','mongodb']
+# }
+# person["colo"]=['black']
+# print(person)
+# person['age']= person["age"] + 1
+# print(person)
+
+# print(person.get('country'))
+
+# print("feforit sabject" in person)
+
+# print(person.items())
+# print(person.keys)
+
+1.
+dog ={}
+
+# 2. 
+dog ={
+'name':'Aikhan',
+'color':'yellou',
+'breed':'bulldog',
+'legs': 4,
+'age':16,
+}
+3.
+students={
+   'ferst_name':'Aikhan',
+    'last_name':'oralbek',
+    'gender':'male',
+    'age': 16,
+    'city':'Almaty',
+    'status':'good',
+    'country':'Kazakstan',
+}
+4.
+students={
+   'ferst_name':'Aikhan',
+    'last_name':'oralbek',
+    'gender':'male',
+    'age': 16,
+    'city':'Almaty',
+    'status':'good',
+    'country':'Kazakstan',
+}
+print(len(students))
+
+5.
+students={
+   'ferst_name':'Aikhan',
+    'last_name':'oralbek',
+    'gender':'male',
+    'age': 16,
+    'city':'Almaty',
+    'status':'good',
+    'country':'Kazakstan',
+}
+print(students[scills])
